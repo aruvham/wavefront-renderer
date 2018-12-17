55ba4028b277b8b3860c660c59261eccd33b60f8
Arturo Ruvalcaba <a.ruvham@gmail.com>
2018-12-16 23:11:59 -0500

    Implement triangle rasterization and back face culling

--- src/index.js
+++ src/index.js
@@ -1,36 +1,76 @@
 import africanHead from './models/african-head';
 import Model from './model';
-import { vec3 } from 'gl-matrix';
-// console.log(vec3, vec3.fromValues(1, 2, 3), vec3.fromValues(4, 5, 6))
+import { vec2, vec3 } from 'gl-matrix';
 
 const model = new Model(africanHead);
+model.setScale(350);
+model.setPosition(vec3.fromValues(400, 400, 0));
+
 const canvasEle = document.querySelector('.canvas');
 const ctx = canvasEle.getContext('2d', { alpha: false });
 
+const renderWireframe = (model, ctx) => {
+  ctx.fillStyle = '#000';
+  ctx.strokeStyle = '#FFF';
+  ctx.fillRect(0, 0, 800, 800);
+  
+  console.time('Render Wireframe');
+  ctx.beginPath();
+  for (let i = 0; i < model.faces.length; i++) {
+    const face = model.faces[i];
+    
+    for (let j = 0; j < 3; j++) {
+      const v0 = model.worldVertices[face[j]]; 
+      const v1 = model.worldVertices[face[(j + 1) % 3]];
+      ctx.moveTo(v0[0], v0[1]);
+      ctx.lineTo(v1[0], v1[1]);
+    }
+  }
+  ctx.stroke();
+  console.timeEnd('Render Wireframe');
+}
+
+// renderWireframe(model, ctx);
 ctx.fillStyle = '#000';
+ctx.strokeStyle = '#FFF';
 ctx.fillRect(0, 0, 800, 800);
 
-ctx.strokeStyle = '#FFF';
-console.time('render');
-ctx.beginPath();
-for (let i = 0; i < model.faces.length; i++) {
-    const face = model.faces[i];
+const lightSource = vec3.fromValues(0, 0, -1);
+console.time('Render');
+var len = model.faces.length;
+for (let i = 0; i < len; i++) {
+  const face = model.faces[i];
 
-    for (let j = 0; j < 3; j++) {
-        const v0 = model.worldVertices[face[j]]; 
-        const v1 = model.worldVertices[face[(j + 1) % 3]];
-        
-        // const x0 = (v0.x * 200) + 250;
-        // const y0 = -(v0.y * 200) + 250;
-        // const x1 = (v1.x * 200) + 250;
-        // const y1 = -(v1.y * 200) + 250;
-        
-        // ctx.moveTo(x0, y0);
-        // ctx.lineTo(x1, y1);
-        ctx.moveTo(v0[0], v0[1]);
-        ctx.lineTo(v1[0], v1[1]);
-    }
+  const v0 = model.modelVertices[face[0]]; 
+  const v1 = model.modelVertices[face[1]]; 
+  const v2 = model.modelVertices[face[2]];
+  
+  var n = vec3.create();
+  var side1 = vec3.create();
+  var side2 = vec3.create();
+
+  vec3.subtract(side1, v2, v0);
+  vec3.subtract(side2, v1, v0);
+
+  vec3.cross(n, side1, side2);
+  vec3.normalize(n, n);
+  
+  var lightIntensity = vec3.dot(n, lightSource);
+
+  // console.log(lightIntensity)
+
+  if (lightIntensity > 0) {
+    const v0 = model.worldVertices[face[0]]; 
+    const v1 = model.worldVertices[face[1]]; 
+    const v2 = model.worldVertices[face[2]];
+
+    ctx.fillStyle = `rgb(${lightIntensity * 255}, ${lightIntensity * 255}, ${lightIntensity * 255})`;
+    ctx.beginPath();
+    ctx.moveTo(v0[0], v0[1]);
+    ctx.lineTo(v1[0], v1[1]);
+    ctx.lineTo(v2[0], v2[1]);
+    ctx.lineTo(v0[0], v0[1]);
+    ctx.fill();
+  }
 }
-ctx.stroke();
-console.timeEnd('render');
-
+console.timeEnd('Render');
--- src/model.js
+++ src/model.js
@@ -1,27 +1,51 @@
-import { vec3 } from 'gl-matrix';
+import { vec2, vec3 } from 'gl-matrix';
+import { runInThisContext } from 'vm';
 
 export default class Model {
-  constructor(file) {
-    this.scale = 350;
-    this.offsetX = 400;
-    this.offsetY = 400;
+  constructor(file, scale = 1, position = vec2.fromValues(0, 0)) {
+    this.scale = scale;
+    this.position = position;
     this.modelVertices = [];
     this.worldVertices = [];
     this.faces = [];
-    this.parseFile(file);
+    this.file = file;
+    this.parseFile();
+  }
+
+  setScale(scale) {
+    this.scale = scale;
+    this.updateWorldVertices();
+  }
+
+  setPosition(position) {
+    this.position = position;
+    this.updateWorldVertices();
+  } 
+
+  updateWorldVertices() {
+    this.worldVertices = [];
+
+    for (let i = 0; i < this.modelVertices.length; i++) {
+      const vertex = this.modelVertices[i];
+      this.worldVertices.push(vec3.fromValues(
+        (vertex[0] * this.scale) + this.position[0],
+        -(vertex[1] * this.scale) + this.position[1],
+        (vertex[2] * this.scale) + this.position[2],
+      ));
+    }
   }
   
-  parseFile(file) {
-    let lines = file.split('\n');
+  parseFile() {
+    let lines = this.file.split('\n');
     lines = lines.map(line => line.split(' '));
     
     lines.forEach(line => {
       if (line[0] === 'v') {
         this.modelVertices.push(vec3.fromValues(line[1], line[2], line[3]));
         this.worldVertices.push(vec3.fromValues(
-          (line[1] * this.scale) + this.offsetX,
-          (line[2] * -this.scale) + this.offsetY,
-          line[3],
+          (line[1] * this.scale) + this.position[0],
+          -(line[2] * this.scale) + this.position[1],
+          (line[2] * this.scale) + this.position[2],
         ));
       } else if (line[0] === 'f') {
         this.faces.push([
